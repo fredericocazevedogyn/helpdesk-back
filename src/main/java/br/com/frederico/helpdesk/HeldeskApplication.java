eacc1511011bfbe691e3c93d24fd0ddd7fa3dc7e
Frederico Carvalho <dentin15@gmail.com>
2022-05-27 15:11:08 -0300

    projeto inciado

---
+++ src/main/java/br/com/frederico/helpdesk/HeldeskApplication.java
@@ -0,0 +1,13 @@
+package br.com.frederico.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeldeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeldeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/frederico/helpdesk/HeldeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frederico.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeldeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
