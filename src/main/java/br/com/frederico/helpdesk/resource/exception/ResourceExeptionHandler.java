722766598bf1544e55dd6dd449269e9e2e17e696
Frederico Carvalho <dentin15@gmail.com>
2022-05-29 18:36:41 -0300

    endpoint findeall para tecnico

--- src/main/java/br/com/frederico/helpdesk/resource/exception/ResourceExeptionHandler.java
+++ src/main/java/br/com/frederico/helpdesk/resource/exception/ResourceExeptionHandler.java
@@ -1,7 +1,6 @@
 package br.com.frederico.helpdesk.resource.exception;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/br/com/frederico/helpdesk/service/TecnicoService.java
+++ src/main/java/br/com/frederico/helpdesk/service/TecnicoService.java
@@ -1,5 +1,6 @@
 package br.com.frederico.helpdesk.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: ID - " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return tecnicoRepository.findAll();
 	}
 	
 }
