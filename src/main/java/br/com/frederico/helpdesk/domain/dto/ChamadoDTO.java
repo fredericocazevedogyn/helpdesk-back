bced8f80c08b51172de21153e90ba46b98626f9f
Frederico Carvalho <dentin15@gmail.com>
2022-05-30 08:40:22 -0300

    Criação das classes de chamado

---
+++ src/main/java/br/com/frederico/helpdesk/domain/dto/ChamadoDTO.java
@@ -0,0 +1,154 @@
+package br.com.frederico.helpdesk.domain.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import br.com.frederico.helpdesk.domain.Chamado;
+
+public class ChamadoDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataAbertura = LocalDate.now();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataFechamento;
+	private Integer prioridade;
+	private Integer status;
+	private String titulo;
+	private String observação;
+	private Integer tecnico;
+	private Integer cliente;
+	private String nomeTecnico;
+	private String nomeCliente;
+	
+	public ChamadoDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ChamadoDTO(Integer id, LocalDate dataAbertura, LocalDate dataFechamento, Integer prioridade, Integer status,
+			String titulo, String observação, Integer tecnico, Integer cliente, String nomeTecnico,
+			String nomeCliente) {
+		super();
+		this.id = id;
+		this.dataAbertura = dataAbertura;
+		this.dataFechamento = dataFechamento;
+		this.prioridade = prioridade;
+		this.status = status;
+		this.titulo = titulo;
+		this.observação = observação;
+		this.tecnico = tecnico;
+		this.cliente = cliente;
+		this.nomeTecnico = nomeTecnico;
+		this.nomeCliente = nomeCliente;
+	}
+	
+	public ChamadoDTO(Chamado obj) {
+		this.id = obj.getId();
+		this.dataAbertura = obj.getDataAbertura();
+		this.dataFechamento = obj.getDataFechamento();
+		this.prioridade = obj.getPrioridade().getCodigo();
+		this.status = obj.getStatus().getCodigo();
+		this.titulo = obj.getTitulo();
+		this.observação = obj.getObservação();
+		this.tecnico = obj.getTecnico().getId();
+		this.cliente = obj.getCliente().getId();
+		this.nomeTecnico = obj.getTecnico().getNome();
+		this.nomeCliente = obj.getCliente().getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDate dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDate getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDate dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Integer getPrioridade() {
+		return prioridade;
+	}
+
+	public void setPrioridade(Integer prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getObservação() {
+		return observação;
+	}
+
+	public void setObservação(String observação) {
+		this.observação = observação;
+	}
+
+	public Integer getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Integer tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Integer getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Integer cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getNomeTecnico() {
+		return nomeTecnico;
+	}
+
+	public void setNomeTecnico(String nomeTecnico) {
+		this.nomeTecnico = nomeTecnico;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	
+ }
---
+++ src/main/java/br/com/frederico/helpdesk/service/ClienteService.java
@@ -0,0 +1,78 @@
+package br.com.frederico.helpdesk.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.frederico.helpdesk.domain.Pessoa;
+import br.com.frederico.helpdesk.domain.Cliente;
+import br.com.frederico.helpdesk.domain.dto.ClienteDTO;
+import br.com.frederico.helpdesk.repository.PessoaRepository;
+import br.com.frederico.helpdesk.repository.ClienteRepository;
+import br.com.frederico.helpdesk.service.exception.DataIntegrityViolationException;
+import br.com.frederico.helpdesk.service.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = clienteRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: ID - " + id));
+	}
+
+	public List<Cliente> findAll() {
+		return clienteRepository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) {
+		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return clienteRepository.save(newObj);
+	}
+
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return clienteRepository.save(oldObj);
+	}
+
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+		}
+
+		clienteRepository.deleteById(id);
+
+	}
+
+	private void validaPorCpfEEmail(ClienteDTO objDTO) {
+
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
+	}
+
+}
